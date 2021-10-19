4edc20bb9b1ddcfa5545a588493b76dac343d665
Benjamin Noufel <benjamin.noufel@gmail.com>
2021-10-19 10:25:22 +0200

    :wrench: Fix rollup config and update http module

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,26 +1,13 @@
-import babel from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
 import remove from "rollup-plugin-delete";
 import {resolve} from "path";
 import {terser} from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-    input: "src/EcommerceHttp.ts",
+    input: resolve("src", "EcommerceHttp.ts"),
     plugins: [
         remove({targets: resolve("lib", "*")}),
         typescript(),
-        commonjs(),
-        nodeResolve(),
-        babel({
-            babelHelpers: "bundled",
-            babelrc: false,
-            presets: [
-                "@babel/preset-env",
-                "@babel/preset-typescript"
-            ]
-        }),
         terser()
     ],
     external: [
@@ -28,7 +15,6 @@ export default {
     ],
     output: {
         file: resolve("lib", "EcommerceHttp.js"),
-        format: "cjs",
-        sourcemap: true
+        format: "cjs"
     }
 };
